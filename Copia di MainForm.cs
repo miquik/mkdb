0488017cdd315b56a42b69a9b43217ee98caec06
miquik <miki_r@libero.it>
2009-03-04 18:20:36 +0100

    re

---
+++ Copia di MainForm.cs
@@ -0,0 +1,187 @@
+﻿/*
+ * Creato da SharpDevelop.
+ * Utente: michele
+ * Data: 02/03/2009
+ * Ora: 11.17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace textops
+{		
+	public class PyParser
+	{
+		private StreamReader _t;
+		
+		public PyParser()
+		{			
+		}
+		
+		public void ParseFile(string filename)
+		{
+			_t = File.OpenText(filename);
+			StringCollection coll = new StringCollection();
+			PySection sec = new PySection();
+			while (!_t.EndOfStream)
+			{
+				coll.Add(_t.ReadLine());
+			}
+			ParseFileSection(coll, sec, 0);
+		}
+		
+		public int ParseFileSection(StringCollection sec, PySection section, int row)
+		{
+			// sec collection, doesn't contain # --- begin / # --- end
+			int count = row;
+			bool insection = false;
+			StringCollection tempc = new StringCollection();
+			foreach (string item in sec)
+			{
+				if (insection == true)
+				{
+					if (item.Contains("# ---") && item.Contains("end"))
+					{
+						insection = false;
+						section.Children.Add(new PySection());
+						PySection child = (PySection)section.Children[parent.Children.Count - 1];
+						child.Begin = count;						
+						int cnt = ParseFileSection(tempc, child, count);	
+					} else {
+						string debugstr = sec[count];
+						tempc.Add(debugstr);						
+					}
+				}				
+				else if (item.Contains("# ---") && item.Contains("begin"))
+				{
+					// begin a new section
+					insection = true;
+					tempc.Clear();
+				}
+				else if (item.Contains("# ---") && item.Contains("end"))
+				{
+				    // something to do here?
+				    insection = false;
+				}
+				else
+				{
+					section.Lines.Add(item);
+				}
+				count++;
+			}
+			return row;
+		}
+	}
+	
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			PyParser parser = new PyParser();
+			parser.ParseFile("../../app_struct.py");
+			/*
+			Match myMatch = Regex.Match(item, "class");
+			if (!myMatch.Success)
+			{
+			}
+			*/
+		}
+	}
+	
+	/*
+	public class PyParser
+	{
+		StringCollection _keywords;
+		string _compiled_keywords;
+		StreamReader _t;
+		
+		public PyParser()
+		{
+			_t = null;
+			_compiled_keywords = "";
+			_keywords = new StringCollection();
+		}
+		
+		public StringCollection Keywords
+		{
+			get	{	return _keywords;	}
+		}
+		
+		public void CompileKeywords()
+		{
+			_compiled_keywords = "";
+			foreach (string it in _keywords)
+			{
+				if (i == Settings.Keywords.Count-1)
+					_compiled_keywords += "\\b" + it + "\\b";
+				else
+					_compiled_keywords += "\\b" + it + "\\b|";
+			}
+		}
+		
+		private int ParseResursive(StreamReader read, PySection parent, int row)
+		{
+			int count = row;
+			bool insection = false;
+			Regex regKeywords = new Regex(_compiled_keywords, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+			Match regMatch,
+			
+			while (insection)
+			{
+				string item = read.ReadLine();
+				regMatch = regKeywords.Match(item);	
+				if (regMatch.Success)
+				{
+					// begin or end??
+					if (item.Contains("begin"))
+					{				
+						// parse sub
+					}
+					if (item.Contains("end"))
+					{
+						insection = false;
+					}
+					count++;
+				}
+			}
+			return count;
+			// count++;			
+		}
+		
+		public bool ParseFile(string filename)
+		{
+			_t = File.OpenText("../../complex_app.py");
+			
+			if (_t == null) return false;
+			
+			int count = 0;
+			string item;
+			while (!t.EndOfStream)
+			{
+				item = t.ReadLine();
+				if (item.Contains("# --- Class begin ---"))
+				{
+					_cur_sec.Children.Add(new PySection());
+					PySection temp = (PySection)_cur_sec.Children[_cur_sec.Children.Count - 1];
+					_cur_sec = temp;
+					_cur_sec.Begin = count;
+				}
+				if (item.Contains("# --- Class end ---"))
+				{
+				}
+				count++;
+			}
+			return true;
+		}
+	}
+	*/
+	
+}
---
+++ Copia di PyClassStruct.cs
@@ -0,0 +1,259 @@
+/*
+ * Creato da SharpDevelop.
+ * Utente: michele
+ * Data: 03/03/2009
+ * Ora: 13.54
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace textops
+{
+	/*
+	 * 0 : # --- begin [Sec1] section ---	B
+	 * 1 : # --- end [Sec1] section ---		E
+	 * 2 : # --- begin [Sec2] section ---	B
+	 * (3) : data 1
+	 * 3 : # --- begin [Sec2] section ---	B
+	 * 
+	 * B = 0, S = 0, E = B + S + 1
+	 * Insert PS = B + 1 + Pos, If (Pos = -1) PS = E;
+	 */
+	
+	public class PySection
+	{
+		private int _begin;
+		private int _size;
+		private	StringCollection _lines;
+		private ArrayList _children;		
+		
+		public PySection()
+		{
+			_begin = 0;
+			_size = 0;
+			_children = new ArrayList();
+			_lines = new StringCollection();
+		}
+		
+		public int Begin
+		{
+			get	{	return _begin;	}
+			set	{	_begin = value;	}
+		}		
+		public int Size
+		{
+			get	{	return _size;	}
+			set	{	_size = value;	}
+		}				
+		public int End
+		{
+			get	{	return _begin + _size + 1;	}
+		}
+		public ArrayList Children
+		{
+			get	{	return _children;	}
+		}
+		public StringCollection Lines
+		{
+			get	{	return _lines;	}
+		}
+		
+		public void AddSection(PySection sec)
+		{
+			_children.Add(sec);
+			Size += sec.Size;
+		}
+		
+		public void RemSection(PySection sec)
+		{
+			_children.Remove(sec);
+			Size -= sec.Size;
+		}
+		
+		
+		public void MoveBeginIndex(int count)
+		{
+			_begin += count;
+			if (_begin < 0) _begin = 0;
+		}
+		
+		public void MoveSizeIndex(int count)
+		{
+			_size += count;
+			if (_size < 0) _size = 0;
+		}
+	}
+	
+	/*
+	public class PyClass
+	{
+		private string _classname;
+		private StringCollection _lines;
+		// pyfile.py
+		private const int _sec_count = 4;
+		public PySection[] _sections;
+		public PyClassSection _cur_section;
+		public int _cur_begin;
+		public int _cur_size;
+		
+		public PyClass()
+		{
+			_lines = new StringCollection();
+			_sections = new PySection[_sec_count];
+			for (int i=0; i<_sec_count; i++)
+			{
+				_sections[i] = new PySection();
+				_sections[i].Begin = 1 + i*2;				
+			}
+			_cur_begin = -1;
+			_cur_size = -1;
+			_cur_section = PyClassSection.PYDEF_NONE;
+		}
+		
+		public string Name
+		{
+			get	{	return _classname;		}
+			set	{	_classname = value;	}
+		}
+		public StringCollection Lines
+		{
+			get	{	return _lines;	}
+		}
+		
+		public void BeginSection(PyClassSection section)
+		{
+			// make this section as current;
+			_cur_section = section;
+			_cur_begin = _sections[(int)section].Begin;
+			_cur_size = _sections[(int)section].Size;
+		}
+		
+		public void EndSection()
+		{
+			// make this section as current;
+			_cur_section = PyClassSection.PYDEF_NONE;
+			_cur_begin = -1;
+			_cur_size = -1;
+		}
+		
+		public void SetBeginLabel(string label)
+		{
+			// _lines[_cur_begin].A
+		}
+		
+		private int SeekToSection(int pos)
+		{
+	 		int B = _cur_begin;
+	 		int E = _cur_begin + _cur_size + 1;
+			
+			int ps = B + pos + 1;
+			if (pos == -1) 	ps = E;
+			if (ps < 0) 	ps = B + 1;
+			if (ps > E) 	ps = E;
+			
+			return ps;
+		}		
+		
+		public void InsertSingleLine(int pos, string str)
+		{
+			// pos : relative to section, -1 mean end of section
+			int row = SeekToSection(pos);
+			if (row >= _lines.Count)
+			{	
+				_lines.Add(str);
+			} else {
+				_lines.Insert(row, str);
+			}
+			MoveSectionSize(1);
+		}
+				
+		public void InsertLines(int pos, string str)
+		{
+			// pos : relative to section, -1 mean end of section
+			string[] arr = Regex.Split(str, "\r\n");
+			int count = arr.Length;
+			int row = SeekToSection(pos);
+			for (int i=0; i<count; i++)
+			{
+				if (row + i >= _lines.Count)
+				{	
+					_lines.Add(arr[i]);
+				} else {
+					_lines.Insert(row + i, arr[i]);
+				}
+			}
+			MoveSectionSize(count);
+		}
+						
+		public void DeleteLine(int pos, int rowcount)
+		{
+			int row = SeekToSection(pos);
+			for (int i=0; i<rowcount; i++)
+			{
+				_lines.RemoveAt(row + i);
+			}
+			MoveSectionSize(-rowcount);
+		}
+		
+		public void ReplaceEntireLine(int pos, string str)
+		{
+			// pos : relative to section, -1 mean end of section
+			int row = SeekToSection(pos);
+			if (row < _lines.Count)
+			{	
+				_lines[row] = str;
+			}
+		}				
+				
+		public void InsertBeforeSection(string str)
+		{
+			// pos : relative to section, -1 mean end of section
+			int row = SeekToSection(0);
+			if (row - 1 < 0)  row = 0; else row--;
+			_lines.Insert(row, str);
+			MoveIndexes(1);
+		}
+		
+		public void InsertAfterSection(string str)
+		{
+			// pos : relative to section, -1 mean end of section
+			int row = SeekToSection( -1);
+			row++;
+			if (row >= _lines.Count)
+			{
+				_lines.Add(str);
+			} else {
+				_lines.Insert(row, str);
+			}
+			for (int i = (int)(_cur_section) + 1; i<_sec_count; i++)
+			{
+				_sections[i].MoveBeginIndex(1);
+			}
+		}				
+				
+		public void MoveIndexes(int count)
+		{
+			for (int i = (int)_cur_section; i<_sec_count; i++)
+			{
+				_sections[i].MoveBeginIndex(count);
+			}
+			_cur_begin = _sections[(int)_cur_section].Begin;
+		}
+		
+		public void MoveSectionSize(int count)
+		{
+			_sections[(int)_cur_section].MoveSizeIndex(count);
+			for (int i = (int)(_cur_section) + 1; i<_sec_count; i++)
+			{
+				_sections[i].MoveBeginIndex(count);
+			}
+			_cur_size = _sections[(int)_cur_section].Size;
+		}
+	}
+	*/
+}
