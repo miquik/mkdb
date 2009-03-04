87e16cf1565c1f9bc8c8328cf96910fe06d781bb
miquik <miki_r@libero.it>
2009-03-05 00:06:01 +0100

    rr

--- Copia di MainForm.cs
+++ Copia di MainForm.cs
@@ -1,40 +1,86 @@
 ﻿/*
  * Creato da SharpDevelop.
- * Utente: michele
+ * Utente: Family Rose
  * Data: 02/03/2009
- * Ora: 11.17
+ * Ora: 20.56
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Drawing;
 using System.Windows.Forms;
 using System.IO;
-using System.Text;
-using System.Text.RegularExpressions;
+using System.Collections.Specialized;
 
-namespace textops
-{		
+namespace testtext
+{
 	public class PyParser
 	{
 		private StreamReader _t;
+		private PySection _main;
 		
 		public PyParser()
 		{			
+		}
+		
+		public void Render(StringCollection box)
+		{
+			RenderSection(box, _main);
+		}
+		
+		private void InsertToPos(StringCollection coll, int pos, string str)
+		{
+			if (pos >= coll.Count)
+			{
+				coll.Add(str);
+			} else {
+				coll.Insert(pos, str);
+			}
+		}
+		
+		public void RenderSection(StringCollection box, PySection section)
+		{
+			if (section.Name != "")
+			{
+				InsertToPos(box, section.Begin, "# --- " + section.Name + " : begin ---\n");
+			}
+			if (section.Children.Count == 0)
+			{
+				int cnt = 0;
+				foreach (string it in section.Lines)
+				{
+					InsertToPos(box, section.Begin + cnt, it + "\n");					
+					cnt++;
+				}
+			} else {
+				int cnt = 0;
+				foreach (string it in section.Lines)
+				{
+					InsertToPos(box, section.Begin + cnt, it + "\n");					
+					cnt++;
+				}
+				foreach (PySection sec in section.Children)
+				{
+					RenderSection(box, sec);
+				}
+			}
+			if (section.Name != "")
+			{
+				InsertToPos(box, section.Begin, "# --- " + section.Name + " : end ---\n");
+			}
 		}
 		
 		public void ParseFile(string filename)
 		{
 			_t = File.OpenText(filename);
 			StringCollection coll = new StringCollection();
-			PySection sec = new PySection();
+			_main = new PySection("");
 			while (!_t.EndOfStream)
 			{
 				coll.Add(_t.ReadLine());
 			}
-			ParseFileSection(coll, sec, 0);
+			ParseFileSection(coll, _main, 0);
 		}
 		
 		public int ParseFileSection(StringCollection sec, PySection section, int row)
@@ -50,10 +96,12 @@ namespace textops
 					if (item.Contains("# ---") && item.Contains("end"))
 					{
 						insection = false;
-						section.Children.Add(new PySection());
-						PySection child = (PySection)section.Children[parent.Children.Count - 1];
+						string nn = item.Substring(6, item.IndexOf(':') - 6);
+						section.Children.Add(new PySection(nn.Trim()));
+						PySection child = (PySection)section.Children[section.Children.Count - 1];
 						child.Begin = count;						
-						int cnt = ParseFileSection(tempc, child, count);	
+						int cnt = ParseFileSection(tempc, child, count);
+						child.Size = cnt - child.Begin;
 					} else {
 						string debugstr = sec[count];
 						tempc.Add(debugstr);						
@@ -78,110 +126,52 @@ namespace textops
 			}
 			return row;
 		}
-	}
+	}	
 	
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
 	public partial class MainForm : Form
 	{
 		public MainForm()
 		{
 			InitializeComponent();
+			StringCollection c = new StringCollection();
 			PyParser parser = new PyParser();
 			parser.ParseFile("../../app_struct.py");
-			/*
-			Match myMatch = Regex.Match(item, "class");
-			if (!myMatch.Success)
+			parser.Render(c);
+			foreach (string item in c)
 			{
+				richTextBox1.AppendText(item);
+				// i++;
+			}
+			/*
+			PyFileEditor ed = new PyFileEditor();
+			ed.InsertSingleLineToSection(-1, PyFileSection.PY_EVENTDECL_SECTION, "Ciao\n");
+			ed.InsertAfterSection(PyFileSection.PY_EVENTIMPL_SECTION, "Ecco dopo\n");
+			ed.InsertAfterSection(PyFileSection.PY_EVENTIMPL_SECTION, "Ecco dopo\n");
+			ed.InsertBeforeSection(PyFileSection.PY_EVENTIMPL_SECTION, "Ecco prima!\n");
+			ed.InsertBeforeSection(PyFileSection.PY_EVENTIMPL_SECTION, "Ecco prima!\n");
+			string[] s = new string[ed.PyStream.Count];
+			int i=0;
+			foreach (string item in ed.PyStream)
+			{
+				s[i++] = item;
+			}
+			for (i=0; i<8; i++)
+			{
+				PySection sec = ed._py_sections[i];
+				string str = s[sec.Begin];
+				str = str.Substring(0, str.Length - 1);
+				s[sec.Begin] = str + " : " + sec.Begin.ToString() + "\n";
+			}
+			i=0;
+			foreach (string item in s)
+			{
+				richTextBox1.AppendText(i.ToString() + " :: " + item);
+				i++;
 			}
 			*/
 		}
 	}
-	
-	/*
-	public class PyParser
-	{
-		StringCollection _keywords;
-		string _compiled_keywords;
-		StreamReader _t;
-		
-		public PyParser()
-		{
-			_t = null;
-			_compiled_keywords = "";
-			_keywords = new StringCollection();
-		}
-		
-		public StringCollection Keywords
-		{
-			get	{	return _keywords;	}
-		}
-		
-		public void CompileKeywords()
-		{
-			_compiled_keywords = "";
-			foreach (string it in _keywords)
-			{
-				if (i == Settings.Keywords.Count-1)
-					_compiled_keywords += "\\b" + it + "\\b";
-				else
-					_compiled_keywords += "\\b" + it + "\\b|";
-			}
-		}
-		
-		private int ParseResursive(StreamReader read, PySection parent, int row)
-		{
-			int count = row;
-			bool insection = false;
-			Regex regKeywords = new Regex(_compiled_keywords, RegexOptions.IgnoreCase | RegexOptions.Compiled);
-			Match regMatch,
-			
-			while (insection)
-			{
-				string item = read.ReadLine();
-				regMatch = regKeywords.Match(item);	
-				if (regMatch.Success)
-				{
-					// begin or end??
-					if (item.Contains("begin"))
-					{				
-						// parse sub
-					}
-					if (item.Contains("end"))
-					{
-						insection = false;
-					}
-					count++;
-				}
-			}
-			return count;
-			// count++;			
-		}
-		
-		public bool ParseFile(string filename)
-		{
-			_t = File.OpenText("../../complex_app.py");
-			
-			if (_t == null) return false;
-			
-			int count = 0;
-			string item;
-			while (!t.EndOfStream)
-			{
-				item = t.ReadLine();
-				if (item.Contains("# --- Class begin ---"))
-				{
-					_cur_sec.Children.Add(new PySection());
-					PySection temp = (PySection)_cur_sec.Children[_cur_sec.Children.Count - 1];
-					_cur_sec = temp;
-					_cur_sec.Begin = count;
-				}
-				if (item.Contains("# --- Class end ---"))
-				{
-				}
-				count++;
-			}
-			return true;
-		}
-	}
-	*/
-	
 }
