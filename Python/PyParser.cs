18d771af6e1feec7bc44e22f6c18b32a4efa6725
miquik <miki_r@libero.it>
2009-03-05 17:59:52 +0100

    Revisione del 05/03/09

---
+++ Python/PyParser.cs
@@ -0,0 +1,127 @@
+/*
+ * Creato da SharpDevelop.
+ * Utente: michele
+ * Data: 05/03/2009
+ * Ora: 9.59
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Collections.Specialized;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace textops
+{
+	public class PyParser
+	{
+		private StreamReader _t;
+		private PySection _main;
+		
+		public PyParser()
+		{			
+		}
+		
+		public void Render(StringCollection box)
+		{
+			RenderSection(box, _main);
+		}
+		
+		private void InsertToPos(StringCollection coll, int pos, string str)
+		{
+			if (pos >= coll.Count)
+			{
+				coll.Add(pos.ToString() + " :: " + str);
+			} else {
+				coll.Insert(pos, pos.ToString() + " :: " + str);
+			}
+		}
+		
+		public void RenderSection(StringCollection box, PySection section)
+		{
+			if (section.Name != "")
+			{
+				string str = "# begin " + section.Name + "\n";
+				InsertToPos(box, section.Begin, str);
+			}
+			if (section.Children.Count == 0)
+			{
+				int cnt = 1;
+				foreach (string it in section.Lines)
+				{
+					InsertToPos(box, section.Begin + cnt, it + "\n");					
+					cnt++;
+				}
+			} else {
+				int cnt = 1;
+				foreach (string it in section.Lines)
+				{
+					InsertToPos(box, section.Begin + cnt, it + "\n");					
+					cnt++;
+				}
+				foreach (PySection sec in section.Children)
+				{
+					RenderSection(box, sec);
+				}
+			}
+			if (section.Name != "")
+			{
+				InsertToPos(box, section.End, "# end " + section.Name + "\n");
+			}
+		}
+		
+		public void ParseFile(string filename)
+		{
+			_t = File.OpenText(filename);
+			StringCollection coll = new StringCollection();
+			_main = new PySection("");
+			while (!_t.EndOfStream)
+			{
+				coll.Add(_t.ReadLine());
+			}
+			ParseFileSection(coll, _main, 0, coll.Count);
+		}
+		
+		public int ParseFileSection(StringCollection sec, PySection section, int begin, int end)
+		{
+			// sec collection, doesn't contain # --- begin / # --- end
+			int count = begin;
+			
+			for (int j=begin; j<end; j++)
+			{
+				string item = sec[j];
+				if (item.Contains("# begin"))
+				{
+					// here's we have a new section
+					string trimstr = item.Trim();
+					string nn = trimstr.Substring(7);
+					section.Children.Add(new PySection(nn.Trim()));
+					PySection child = (PySection)section.Children[section.Children.Count - 1];
+					child.Begin = count;
+					// find the end...
+					string ends = item.Replace("begin", "end");
+					int end_idx = sec.IndexOf(ends);
+					if ((end_idx >= begin) || (end_idx <= end))
+					{					
+						child.Size = end_idx - child.Begin - 1;
+						ParseFileSection(sec, child, count + 1, end_idx);
+						j = end_idx;
+						count = end_idx;
+					} else {
+						// Not found error...
+					}
+				} else if (item.Contains("# end"))
+				{
+					// DO NOTHING
+				} else
+				{
+					section.Lines.Add(item);					
+				}
+				count++;
+			} 
+			return count;
+		}		
+		
+	}		
+}
